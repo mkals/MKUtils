602f72c194852d0c126c748314679855d4eaed8d
Morten Kals <morten@kals.no>
2022-10-10 11:50:37 +0100

    added configure logger

--- src/MKUtils/__init__.py
+++ src/MKUtils/__init__.py
@@ -1,13 +1,5 @@
-import collections
-from dataclasses import dataclass
-import re
 from pathlib import Path
-from enum import Enum
-from abc import ABC
-import os
-from pathlib import Path
-import time
-import platform
+import os, logging
 
 HOME_DIRECTORY = Path.home()
 DATA_DIRECTORY = os.path.join(HOME_DIRECTORY, 'data')
@@ -21,3 +13,26 @@ def append_to_file(file, l):
 def generate_directory(directory):
 	if not os.path.exists(directory):
 		os.makedirs(directory)
+
+
+'''
+Convenience method for setting up logger. 
+Must be called before logging is tarted.
+Must be called again from pools in muiltiprocessing module, works fine to have all write to same file.
+'''
+def configure_logger(dir: str, filename: str, label: str, file_mode: str, file_level: str = 'DEBUG', stream_level: str = 'INFO'):
+    file_handler = logging.FileHandler(os.path.join(dir, filename), mode=file_mode)
+    file_handler.setLevel(file_level)
+    
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(stream_level)
+
+    logging.basicConfig(
+        format=f'%(asctime)s,%(msecs)d %(name)s %(levelname)s {label} %(message)s', # label to mark which process was logging
+        datefmt='%H:%M:%S',
+        level=logging.DEBUG,
+        handlers=[file_handler, stream_handler],
+    )
+    
+    loggers = ["PIL.TiffImagePlugin", "PIL.PngImagePlugin", "matplotlib.font_manager", "matplotlib.axes"]
+    for l in loggers: logging.getLogger(l).level = logging.WARN
